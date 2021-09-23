9fbbdd589709066fb5c69756a6c702e2d2a0c6ad
David <davidphang431@gmail.com>
2021-09-23 15:38:08 +0700

    Add some comments

--- technical/leetcode/easy_02_validBraces.js
+++ technical/leetcode/easy_02_validBraces.js
@@ -1,30 +1,35 @@
 // check if braces is valid
 
+// array to check if it contains valid opening and closing
 const opening = ["(", "[", "{"];
 const closing = [")", "]", "}"];
 
 function isValid(braces) {
-  const bracesArray = braces.split("");
-  const validArray = [];
-  let validMatches = 0;
+  const bracesArray = braces.split(""); // first split the string into array
+  const validArray = []; // create a temp array to store it
+  let validMatches = 0; // create a temp var to check if there's a valid number of opening and closing
 
   for (let i = 0; i < bracesArray.length; i++) {
+    // do a loop
     if (opening.includes(bracesArray[i])) {
-      validArray.push(bracesArray[i]);
-      validMatches++;
+      // if found an opening
+      validArray.push(bracesArray[i]); // push it to the temp array
+      validMatches++; // and increase validMatches by 1
     }
 
     if (closing.includes(bracesArray[i])) {
-      const latestOpening = validArray[validArray.length - 1];
+      // if instead you found a closing
+      const latestOpening = validArray[validArray.length - 1]; // check what's the latest opening in the validArray
       if (opening.indexOf(latestOpening) !== closing.indexOf(bracesArray[i])) {
-        return false;
+        // if the index in the opening (latestOpening) and closing(bracesArray[i]) is not the same
+        return false; // return false, because it means something else closes first
       }
-      validMatches--;
-      validArray.pop();
+      validMatches--; // and decrease the validMatches by 1
+      validArray.pop(); // and pop the latest element in the validArray because that mean it already closes
     }
   }
 
-  return true && validMatches === 0;
+  return validMatches === 0;
 }
 
 console.log(isValid("(){}[]")); // true
