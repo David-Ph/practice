e9c21fa79d74f08fd6b236271e9da3e3df5a4d5b
David <davidphang431@gmail.com>
2021-10-13 14:53:10 +0700

    More on regex

--- technical/regex/00_cheatsheet.js
+++ technical/regex/00_cheatsheet.js
@@ -3,5 +3,12 @@ let caseInsensitive = /regex/i; // i = case insensitive
 let globalMatches = /regex/g; // g = global / more than one matches
 
 // ? expresions
-
 let matchStrings = /strings/gi; // a regex that will match string called 'words'
+let matchLetters = /[a-z]/gi; // a regex that will match letters a to z
+let matchSeverals = /[bcm]at/gi; // will match bat, cat, mat
+let matchWildCard = /[.un]/gi; // will match *un like fun, bun, mun, sun, etc
+let matchNumbers = /[0-6]/gi; // match number 0 to 6
+let matchOccurances = /a+/gi; // match one or more occurances of a
+let matchZeroOrMore = /go*/; // match zero or more occurances of g + o
+
+let negateExpressions = /[^aiueo]/gi; // match everything but aiueo
