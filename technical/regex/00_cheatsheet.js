92d4484d3d7d296a79cf3c9574de7df18983c331
David <davidphang431@gmail.com>
2021-10-15 16:59:21 +0700

    More regex and codewars

--- technical/regex/00_cheatsheet.js
+++ technical/regex/00_cheatsheet.js
@@ -16,6 +16,10 @@ let lastRegex = /caboose$/; // will match caboose in the end of the string
 let shortHand = /\w/g; // match alphanumeric shorthand for /[a-zA-Z0-9_]/
 let digitShortHand = /\d/g; // match every number shorthand for /[0-9]/
 let nonDigit = /\D/g; // match every non number, shorthand for /[^0-9]/
+let whiteSpaces = /\s/g; // match every whitespaces
+let nonWhiteSpaces = /\S/g; // match every non white spaces
+let multipleA = /a{3,5}h/; // in ah string, only match those who has a that apepars between 3-5 times
+let lowerCount = /haz{4,}ah/i; // in hazah string, only match those who has z that appears more than 4 times
 
 let negateExpressions = /[^aiueo]/gi; // match everything but aiueo
 let negateBeginning = /^Cal/gi; // will negate only Cal at the beginning
