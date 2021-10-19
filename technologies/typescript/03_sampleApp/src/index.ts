e75ea57ef00a564703f0ba4905ef30a71a9304dd
David <davidphang431@gmail.com>
2021-10-19 17:03:29 +0700

    Add error middleware to sampleApp

--- technologies/typescript/03_sampleApp/src/index.ts
+++ technologies/typescript/03_sampleApp/src/index.ts
@@ -6,6 +6,9 @@ import express from "express";
 import cors from "cors";
 import helmet from "helmet";
 import { ItemsRouter } from "./items/items.router";
+import { errorHandler } from "./middleware/error.middleware";
+import { notFoundHandler } from "./middleware/not-found.middleware";
+
 dotenv.config();
 
 /**
@@ -24,6 +27,9 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use("/api/menu/items", ItemsRouter);
+
+app.use(notFoundHandler);
+app.use(errorHandler);
 
 /**
  * Server Activation
---
+++ technologies/typescript/03_sampleApp/src/middleware/not-found.middleware.ts
@@ -0,0 +1,9 @@
+import { Request, Response, NextFunction } from "express";
+
+export const notFoundHandler = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  res.status(404).send("Endpoint Not Found");
+};
