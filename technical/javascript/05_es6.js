2c71c1d89d6e2ef109cd4b7302645b86ff3995ec
David <davidphang431@gmail.com>
2021-10-23 15:46:31 +0700

    Add spread operator

--- technical/javascript/05_es6.js
+++ technical/javascript/05_es6.js
@@ -25,3 +25,24 @@ function addFunction(a, b) {
 }
 
 const addArrow = (a, b) => a * b;
+
+// spread operator
+const hobbies = ["Piano", "Dota"];
+const activeHobbies = ["Hiking", ...hobbies];
+
+activeHobbies.push(...hobbies); // hobbies will return its element one by one
+let string = "hello";
+string = [...string];
+
+const person = {
+  name: "Max",
+  age: 30,
+};
+
+const copyReference = person; // this actually copies the pointer/reference, and not the object
+const copyPerson = { ...person };
+
+var person = { name: "Max" };
+var newPerson = person;
+newPerson.name = "Anna";
+console.log(person.name); // will print Anna
