e0b0de4a7839f9d9d41aa2dc2ff93f92c129295f
David <davidphang431@gmail.com>
2021-10-23 15:32:28 +0700

    Add var let const

---
+++ technical/javascript/05_var-let-const.js
@@ -0,0 +1,19 @@
+let d = b;
+// let c = a;
+
+// let a = 1;
+var b = 2;
+
+// console.log(c);
+console.log(d);
+
+let variable = 5;
+variable = 10;
+
+if (5 > 1) {
+  var isBad = true;
+  let isGood = false;
+}
+
+console.log(isBad); // true
+console.log(isGood); // error
