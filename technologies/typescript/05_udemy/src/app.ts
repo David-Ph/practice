da3d3e6f4f56f7125c021dee745defeba00d7f45
David <davidphang431@gmail.com>
2021-10-25 12:59:47 +0700

    Add inheritance for TS

--- technologies/typescript/05_udemy/src/app.ts
+++ technologies/typescript/05_udemy/src/app.ts
@@ -1,12 +1,6 @@
 class Department {
   //   private name: string; // NOT A KEY VALUE PAIR
   private employees: string[] = []; // now employees is only accessible from inside the class.
-  //contructor is a function that is executed when
-  // a new instance is created, basically it allows us to
-  // initialize the property
-  //   constructor(n: string) {
-  //     this.name = n;
-  //   }
 
   // this is a shorthand constructor
   constructor(private readonly id: string, public name: string) {}
@@ -31,16 +25,43 @@ class Department {
   }
 }
 
-const BackEnd = new Department("fuckingid", "Back-end engineer");
+class ITDepartment extends Department {
+  admins: string[];
+  constructor(id: string, admins: string[]) {
+    super(id, "IT");
+    this.admins = admins;
+  }
+}
+
+class AccountingDepartment extends Department {
+  constructor(id: string, private reports: string[] = []) {
+    super(id, "Accounting");
+  }
+
+  addReport(report: string) {
+    this.reports.push(report);
+  }
+
+  printReports() {
+    console.log(this.reports);
+  }
+}
+
+// const BackEnd = new Department("fuckingid", "Back-end engineer");
+const BackEnd = new ITDepartment("fuckingid", ["Jack"]);
 
 BackEnd.describe();
 BackEnd.addEmployee("Bambang");
 BackEnd.addEmployee("Charlie");
 BackEnd.addEmployee("Alice");
 BackEnd.printEmployees();
+// console.log(BackEnd);
 
 const backEndCopy = { describe: BackEnd.describe };
 // backEndCopy.describe(); // Error, because backEndCopy is not of type Department
-
 const newCopy = { name: "Dummy", describe: BackEnd.describe, size: 25 };
 // newCopy.describe(); // will work
+
+const Accounting = new AccountingDepartment("newDepartment");
+Accounting.addReport("SHIT WENT WRONG");
+Accounting.printReports();
