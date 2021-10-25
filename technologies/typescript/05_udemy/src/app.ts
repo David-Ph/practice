b20b0ca2cccf616061dbaf2abfee60bd8010e0e6
David <davidphang431@gmail.com>
2021-10-25 15:51:35 +0700

    optional properties in interface, classes implements interface, interface extends interface

--- technologies/typescript/05_udemy/src/app.ts
+++ technologies/typescript/05_udemy/src/app.ts
@@ -1,11 +1,27 @@
-interface Greetable {
+// we can also use interface as a custom function type
+// type AddFn = (a:number, b:number) => number;
+interface AddFn {
+  (a: number, b: number): number;
+}
+const add: AddFn = (n1: number, n2: number) => {
+  return n1 + n2;
+};
+
+// /////////////////////////////////////
+
+interface Names {
   readonly name: string;
+  outputName?: string; // this makes outputName to be optional
+}
+
+interface Greetable extends Names {
   greet(phrase: string): void;
 }
 
 class Person implements Greetable {
   name: string;
   age = 30;
+  outputName = "My Name";
 
   constructor(n: string) {
     this.name = n;
