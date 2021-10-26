cc6d275219b813a5f5be1368f1a337a787c9eff7
David <davidphang431@gmail.com>
2021-10-26 17:31:31 +0700

    add funcction overload

--- technologies/typescript/05_udemy/src/intersection.ts
+++ technologies/typescript/05_udemy/src/intersection.ts
@@ -29,15 +29,28 @@ type Universal = Combinable & Numeric;
 
 // const newUniversal: Universal = 'hello';
 
-function addCombineable(a: Combinable, b: Combinable) {
+// function overload
+// we can use this when we have a function
+// that can accept multiple possibilities of paramters and return types
+function addCombineable(a: string): string;
+function addCombineable(a: string, b: string): string;
+function addCombineable(a: number, b: number): number;
+function addCombineable(a: number, b: string): string;
+function addCombineable(a: string, b: number): string;
+function addCombineable(a: Combinable, b?: Combinable) {
   // this is a type guard
   // where we check type safety ourselves
   // usually used for union types flexibility where the type can be number or string
+  if (!b) return a.toString();
+
   if (typeof a === "string" || typeof b === "string") {
     return a.toString() + b.toString();
   }
+
   return a + b;
 }
+
+const result = addCombineable(5, "5");
 
 function printEmployee(emp: EitherEmployee) {
   console.log(emp.name);
