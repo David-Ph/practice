82070a75db625b022d108b4aad4c3a83cd38e78c
David <davidphang431@gmail.com>
2021-10-26 12:41:26 +0700

    Update redeemPoint

--- technical/01_practice/redeemPoint.js
+++ technical/01_practice/redeemPoint.js
@@ -1,29 +1,33 @@
-const vouchers = [
-  { price: "10rb", point: 100 },
-  { price: "25rb", point: 200 },
-  { price: "50rb", point: 400 },
-  { price: "100rb", point: 800 },
+let vouchers = [
+  { price: "10rb, ", point: 100 },
+  { price: "25rb, ", point: 200 },
+  { price: "50rb, ", point: 400 },
+  { price: "100rb, ", point: 800 },
 ];
 
 function redeemPoint(point) {
   let getVouchers = {};
 
-  for (let i = vouchers.length - 1; i >= 0; i--) {
-    while (point >= vouchers[i].point) {
-      point -= vouchers[i].point;
+  vouchers = vouchers.sort((a, b) => b.point - a.point);
 
-      if (!getVouchers[vouchers[i].price]) {
-        getVouchers[vouchers[i].price] = 0;
+  for (const element of vouchers) {
+    while (point >= element.point) {
+      point -= element.point;
+
+      if (!getVouchers[element.price]) {
+        getVouchers[element.price] = 0;
       }
-      getVouchers[vouchers[i].price]++;
+
+      getVouchers[element.price]++;
     }
   }
 
   let string = "Voucher ";
-  for (const key in getVouchers) {
-    string += `${key}, `;
+  for (let [key, value] of Object.entries(getVouchers)) {
+    string += `${key.repeat(value)}`;
   }
   return (string += `dengan sisa point adalah ${point}p`);
 }
 
 console.log(redeemPoint(1150)); // Voucher 100rb, 25rb, 10rb, dengan sisa point adalah 50p
+console.log(redeemPoint(2000)); // Voucher 100rb, 100rb, 50rb, dengan sisa point adalah 0p
