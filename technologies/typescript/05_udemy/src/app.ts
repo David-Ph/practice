7835c0408492cf36c0cd2f1dd1ab28a5bbd790a9
David <davidphang431@gmail.com>
2021-10-27 13:54:33 +0700

    Progress on generics TS

--- technologies/typescript/05_udemy/src/app.ts
+++ technologies/typescript/05_udemy/src/app.ts
@@ -7,6 +7,86 @@ const promise: Promise<string> = new Promise((resolve, reject) => {
 });
 
 promise.then((data) => {
-  console.log(data.split(""));
+  // console.log(data.split(""));
   //   Math.floor(data); // ERRROR
 });
+
+// /////////////////////////////
+// Generics
+// the types are not defined when we delcare the function
+// but when we call the function
+// you can also put a constraint on the generics type
+function merge<T extends object, U extends object>(a: T, b: U) {
+  return Object.assign(a, b);
+}
+
+const mergedObj = merge({ name: "Max", hobbies: ["Sports"] }, { age: 30 });
+const mergedObj2 = merge(
+  { name: "Max", hobbies: ["Sports"] },
+  { age: 30, height: 175 }
+);
+// const mergedObj3 = merge({ name: "Max", hobbies: ["Sports"] }, 30);
+
+console.log(mergedObj);
+
+interface hasLength {
+  length: number;
+}
+
+function countAndDescribe<T extends hasLength>(element: T) {
+  let description = "Has no value.";
+  if (element.length > 0) {
+    description = `Has ${element.length} elements`;
+  }
+
+  return [element, description];
+}
+
+console.log(countAndDescribe(["Sports", "Cooking"]));
+
+const objects = {
+  name: "max",
+  age: 30,
+  height: 175,
+};
+
+for (const [key, value] of Object.entries(objects)) {
+}
+
+function extractAndConvert<T extends object, U extends keyof T>(
+  obj: T,
+  key: U
+) {
+  return `Value : ${obj[key]}`;
+}
+
+console.log(extractAndConvert({ name: "Max" }, "name"));
+
+type primitives = string | number | boolean;
+// we want to make sure that T is at least a primitive data value
+// because we don't want to accidentally pass an object
+// because objects passes by reference, and not value
+class DataStorage<T extends primitives> {
+  private data: T[] = [];
+
+  addItem(item: T) {
+    this.data.push(item);
+  }
+
+  removeItem(item: T) {
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+
+  getItems() {
+    return [...this.data];
+  }
+}
+
+const textStorage = new DataStorage<string>();
+textStorage.addItem("First Text");
+textStorage.addItem("Second Text");
+console.log(textStorage.getItems());
+
+const numberStorage = new DataStorage<number>();
+numberStorage.addItem(50);
+// numberStorage.addItem("50"); // error
--- technologies/typescript/notes_04.js
+++ technologies/typescript/notes_04.js
@@ -13,4 +13,19 @@ singleton pattern is where you want to make sure that a class can only be create
 * Class vs Interface
 You usually use Class to create a new instance of an object with methods
 you usually use interface to enfore a Type of something
+
+* interface vs types
+very similar in use cases, but there are some differences:
+
+Use type when defining an alias for primitive types (string, boolean, number, bigint, symbol, etc)
+Use type when defining tuple types
+Use type when defining function types
+Use type when defining a union
+Use type when trying to overload functions in object types via composition
+Use type when needing to take advantage of mapped types
+
+
+Use interface for all object types where using type is not required (see above)
+Use interface when you want to take advatange of declaration merging.
+
 */
