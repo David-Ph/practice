0de5ecde734c840fb90edaad55b1aa2a915a5b85
David <davidphang431@gmail.com>
2021-11-04 14:20:48 +0700

    Update docker notes

--- technologies/docker/note.js
+++ technologies/docker/note.js
@@ -8,6 +8,7 @@
         - docker ps -> check running container
         - docker image ls -> check all images
         - docker stop [container id] -> stop docker container
+        - sudo docker kill $(sudo docker ps -q)
         - docker run -p 8000:3000 -d docker-gb13 -> to run a docker container in detached state
         - docker ps -a -> to check all containers
         - docker container rm [CONTAINER ID] -> remove all containers
