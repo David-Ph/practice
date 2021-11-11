aeb4d60086e07356d178ed13d7dae7f1b4af28f9
David <davidphang431@gmail.com>
2021-11-11 14:47:02 +0700

    update readme

--- bash.js
+++ bash.js
@@ -1,2 +1,7 @@
 // to edit host file
-// ? sudo nano /etc/hosts
+//? sudo nano /etc/hosts
+
+// to set a variable to be available
+//? export {VARIABLE_NAME} = {VARIABLE_VALUE}
+// example
+//? export GIT_PACKAGE_AUTH_TOKEN=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
