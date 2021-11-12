ebabfb9a9bbddb6130c85090e2c960c829ca3664
David <davidphang431@gmail.com>
2021-11-12 15:35:42 +0700

    Intro to Laravel

---
+++ technologies/laravel/coba/coba2-laravel/resources/views/about.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>myBlog | About</title>
+    <link rel="stylesheet" href="css/style.css">
+    <script src="js/index.js" defer></script>
+</head>
+
+<body>
+    <h1>About</h1>
+    <h3>Hi! I'm <?= $name; ?>!</h3>
+    <p><?= $email; ?></p>
+    <p>I'm a <?= $job; ?></p>
+</body>
+
+</html>
--- technologies/laravel/coba/coba2-laravel/routes/web.php
+++ technologies/laravel/coba/coba2-laravel/routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "David",
+        "job" => "Jr Software Engineer",
+        "email" => "davidphang431@gmail.com"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
