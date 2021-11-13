4e810b6e26ce84a7c56c6f0abf8e0bbad2e9cb0f
David <davidphang431@gmail.com>
2021-11-13 17:29:24 +0700

    Add user form handler

--- technologies/react/note.js
+++ technologies/react/note.js
@@ -9,4 +9,21 @@ useState is a function provided from React library, it allows us to define value
 
 useState needs a default value, for example
 ? const [title, setTitle] = useState(props.title);
+
+you can also useState to an object
+const [input, setInput]useState = ({
+    title: "",
+    amount: "",
+    date: ""
+})
+
+setInput({
+    ...input,
+    title = event.target.value
+})
+
+whenever your setState functions depends on the previous state, you should do it like this
+setInput((prevState)=>{
+    return {...prevState, title: event.target.value}
+})
 */
