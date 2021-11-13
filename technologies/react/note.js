8351fc30cbbddc06f8b6d28eb9a33b524e21b757
David <davidphang431@gmail.com>
2021-11-13 16:09:44 +0700

    react usestate

--- technologies/react/note.js
+++ technologies/react/note.js
@@ -4,5 +4,9 @@ With react, you can create a component that wraps other html or component elemen
 in that case, you need to set {props.children} or {props.className}
 in the wrapper component
 
+? import {useState} from 'React';
+useState is a function provided from React library, it allows us to define values as state, and any change to those values should force the component function to rerender again
 
+useState needs a default value, for example
+? const [title, setTitle] = useState(props.title);
 */
