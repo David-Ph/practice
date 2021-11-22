88bd10f96890ed82bb00140228a5e6d9da8192b6
David <davidphang431@gmail.com>
2021-11-22 18:09:21 +0700

    Fix read more image

--- technologies/laravel/coba/coba2-laravel/resources/views/post.blade.php
+++ technologies/laravel/coba/coba2-laravel/resources/views/post.blade.php
@@ -12,8 +12,17 @@
                         href="/blog?author={{ $post->author->username }}">{{ $post->author->name }}</a> in <a
                         href="/blog?category={{ $post->category->slug }}">{{ $post->category->name }}</a></h5>
 
-                <img src="https://source.unsplash.com/1200x400?${{ $post->category->name }}" class="img-fluid"
-                    alt="{{ $post->category->name }}">
+                @if ($post->image)
+                    <div style="max-height: 400px; overflow: hidden;">
+                        <img src="{{ asset('storage/' . $post->image) }}" class="img-fluid mt-3"
+                            alt="{{ $post->category->name }}">
+                    </div>
+                @else
+                    <img src="https://source.unsplash.com/1200x400?${{ $post->category->name }}" class="img-fluid"
+                        alt="{{ $post->category->name }}">
+                @endif
+
+
 
                 <article class="my-3 fs-5">
                     {!! $post->body !!}
