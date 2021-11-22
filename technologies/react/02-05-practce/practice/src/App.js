6d990b0b56b760eb73e434135ce67b37fe1a1623
David <davidphang431@gmail.com>
2021-11-22 10:17:55 +0700

    add goal

--- technologies/react/02-05-practce/practice/src/App.js
+++ technologies/react/02-05-practce/practice/src/App.js
@@ -1,8 +1,26 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+
+import CourseInput from "./components/CourseGoals/CourseInput/CourseInput";
+
+import "./App.css";
 
 function App() {
-  return;
+  const [goals, setGoals] = useState([]);
+
+  const addGoalHandler = (goal) => {
+    setGoals((prevState) => {
+      return [goal, ...prevState];
+    });
+    console.log(goals)
+  };
+
+  return (
+    <div>
+      <section id="goal-form">
+        <CourseInput onAddGoal={addGoalHandler} />
+      </section>
+    </div>
+  );
 }
 
 export default App;
