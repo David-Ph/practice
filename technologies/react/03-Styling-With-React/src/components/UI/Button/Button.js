86137ba872a33309067fd66d8c0f586328da444d
David <davidphang431@gmail.com>
2021-11-23 09:11:56 +0700

    Styled components media queries

--- technologies/react/03-Styling-With-React/src/components/UI/Button/Button.js
+++ technologies/react/03-Styling-With-React/src/components/UI/Button/Button.js
@@ -5,13 +5,18 @@ import styled from "styled-components";
 // this is called attacked template literal
 // what we pass in that backticks will end up into that button method
 const Button = styled.button`
-    font: inherit;
-    padding: 0.5rem 1.5rem;
-    border: 1px solid #8b005d;
-    color: white;
-    background: #8b005d;
-    box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
-    cursor: pointer;
+  font: inherit;
+  padding: 0.5rem 1.5rem;
+  border: 1px solid #8b005d;
+  color: white;
+  background: #8b005d;
+  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+  cursor: pointer;
+  width: 100%;
+
+  @media(min-width: 768px){
+    width: auto;
+  }
 
   &:focus {
     outline: none;
