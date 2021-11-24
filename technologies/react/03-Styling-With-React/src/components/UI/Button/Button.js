2e76ec05a62ff08f6b2c9a1a1f5b0fdb6e90233e
David <davidphang431@gmail.com>
2021-11-24 09:25:47 +0700

    React CSS modules

--- technologies/react/03-Styling-With-React/src/components/UI/Button/Button.js
+++ technologies/react/03-Styling-With-React/src/components/UI/Button/Button.js
@@ -1,41 +1,42 @@
-// import React from "react";
-import styled from "styled-components";
+import React from "react";
+// import styled from "styled-components";
 
-// import "./Button.css";
+// THIS IS CSS MODULES
+import styles from "./Button.module.css";
 // this is called attacked template literal
 // what we pass in that backticks will end up into that button method
-const Button = styled.button`
-  font: inherit;
-  padding: 0.5rem 1.5rem;
-  border: 1px solid #8b005d;
-  color: white;
-  background: #8b005d;
-  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
-  cursor: pointer;
-  width: 100%;
+// const Button = styled.button`
+//   font: inherit;
+//   padding: 0.5rem 1.5rem;
+//   border: 1px solid #8b005d;
+//   color: white;
+//   background: #8b005d;
+//   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+//   cursor: pointer;
+//   width: 100%;
 
-  @media(min-width: 768px){
-    width: auto;
-  }
+//   @media(min-width: 768px){
+//     width: auto;
+//   }
 
-  &:focus {
-    outline: none;
-  }
+//   &:focus {
+//     outline: none;
+//   }
 
-  &:hover,
-  &:active {
-    background: #ac0e77;
-    border-color: #ac0e77;
-    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
-  }
-`;
+//   &:hover,
+//   &:active {
+//     background: #ac0e77;
+//     border-color: #ac0e77;
+//     box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+//   }
+// `;
 
-// const Button = props => {
-//   return (
-//     <button type={props.type} className="button" onClick={props.onClick}>
-//       {props.children}
-//     </button>
-//   );
-// };
+const Button = props => {
+  return (
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+};
 
 export default Button;
