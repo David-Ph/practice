bf92ad3f3ecf7c4e461df2ded862c2c245fe0041
David <davidphang431@gmail.com>
2021-12-10 14:29:29 +0700

    Add Item CRUD

--- technologies/typescript/12_glints-assessment-in-TS/src/controllers/ItemController.ts
+++ technologies/typescript/12_glints-assessment-in-TS/src/controllers/ItemController.ts
@@ -1,21 +1,14 @@
 import { Request, Response, NextFunction, RequestHandler } from "express";
 import { Item } from "../models/index";
 
-type NumberOrUndefined = number | undefined;
-
 class ItemController {
   async getItems(req: Request, res: Response, next: NextFunction) {
     try {
       // price and stock filter
-      let priceMin = 0;
-      let priceMax = 1000000000;
-      let stockMin = 0;
-      let stockMax = 100000;
-
-      if(req.query.price_min) priceMin = +req.query.price_min; 
-      if(req.query.price_max) priceMax = +req.query.price_max; 
-      if(req.query.stock_min) stockMin = +req.query.stock_min; 
-      if(req.query.stock_max) stockMax = +req.query.stock_max; 
+      let priceMin = +req.query.price_min! || 0;
+      let priceMax = +req.query.price_max! || 1000000000;
+      let stockMin = +req.query.stock_min! || 0;
+      let stockMax = +req.query.stock_max! || 100000;
 
       const query: any = {
         price: { $gte: priceMin, $lte: priceMax },
@@ -25,16 +18,14 @@ class ItemController {
       if (req.query.category) query.category = req.query.category;
 
       // sorting
-      const sortField = req.query.sortBy as string || "created_at";
-      const orderBy = req.query.orderBy as string || "desc";
+      const sortField = (req.query.sortBy as string) || "created_at";
+      const orderBy = (req.query.orderBy as string) || "desc";
 
       // pagination
-      const page = req.query.page as NumberOrUndefined || 0;
-      const limit = req.query.limit as NumberOrUndefined || 5;
-      const skipCount = page > 0 ? (page as number - 1) * limit : 0;
-
-      console.log(query);
-
+      const page = +req.query.page! || 0;
+      const limit = +req.query.limit! || 5;
+      const skipCount = page > 0 ? ((page as number) - 1) * limit : 0;
+      
       // find data
       const data = await Item.find(query)
         .sort({ [sortField]: orderBy })
@@ -48,7 +39,72 @@ class ItemController {
       }
 
       res.status(200).json({ data, count });
+    } catch (error) {
+      next(error);
+    }
+  }
 
+  async getOneItem(req: Request, res: Response, next: NextFunction) {
+    try {
+      const data = await Item.findOne({ _id: req.params.id });
+
+      if (!data) {
+        return next({ message: "No items found", statusCode: 404 });
+      }
+
+      res.status(200).json({ data });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async createItem(req: Request, res: Response, next: NextFunction) {
+    try {
+      const data = await Item.create(req.body);
+
+      res.status(201).json({ data });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async updateItem(req: Request, res: Response, next: NextFunction) {
+    try {
+      const data = await Item.findOneAndUpdate(
+        { _id: req.params.id },
+        req.body,
+        { new: true }
+      );
+
+      res.status(201).json({ data });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async updateStock(req: Request, res: Response, next: NextFunction) {
+    try {
+      const data = await Item.findOneAndUpdate(
+        { _id: req.params.id },
+        { stock: req.body.stock },
+        { new: true }
+      );
+
+      res.status(201).json({ data });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async deleteItem(req: Request, res: Response, next: NextFunction) {
+    try {
+      const data = await Item.findOneAndDelete({ _id: req.params.id });
+
+      if (!data) {
+        return next({ statusCode: 404, message: "Item not found" });
+      }
+
+      res.status(200).json({ message: "Item successfully deleted" });
     } catch (error) {
       next(error);
     }
--- technologies/typescript/12_glints-assessment-in-TS/src/routes/ItemsRouter.ts
+++ technologies/typescript/12_glints-assessment-in-TS/src/routes/ItemsRouter.ts
@@ -3,5 +3,11 @@ import {ItemService} from "../controllers/ItemController";
 const router = Router();
 
 router.get('/', ItemService.getItems);
+router.get('/detail/:id', ItemService.getOneItem);
+router.post('/', ItemService.createItem);
+router.put('/update/:id', ItemService.updateItem);
+router.put('/update-stock/:id', ItemService.updateStock);
+router.delete('/delete/:id', ItemService.deleteItem);
+
 
 export { router as ItemsRouter };
