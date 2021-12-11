1ae0248e799c1c59a84b761968b0ee93b39b3bc5
David <davidphang431@gmail.com>
2021-12-11 18:13:29 +0700

    Add cart item componente

--- technologies/react/09-practice-project/src/components/Cart/Cart.js
+++ technologies/react/09-practice-project/src/components/Cart/Cart.js
@@ -7,10 +7,13 @@ import classes from "./Cart.module.css";
 function Cart(props) {
   const CartCtx = useContext(CartContext);
 
+  const totalAmount = `$${CartCtx.totalAmount.toFixed(2)}`;
+  const hasItems = CartCtx.items.length > 0;
+
   const cartItems = (
-    <ul>
+    <ul className={classes["cart-items"]}>
       {CartCtx.items.map((item) => (
-        <li className={classes["cart-items"]}>{item.name}</li>
+        <li>{item.name}</li>
       ))}
     </ul>
   );
@@ -20,13 +23,13 @@ function Cart(props) {
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
-        <span>35.66</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
         <button onClick={props.onHideCart} className={classes["button--alt"]}>
           Close
         </button>
-        <button className={classes.button}>Order</button>
+        {hasItems && <button className={classes.button}>Order</button>}
       </div>
     </Modal>
   );
