8c7aee5839641e04fe1651353194c138a451e9de
David <davidphang431@gmail.com>
2021-12-11 17:35:50 +0700

    Add cart reducer

--- technologies/react/09-practice-project/src/components/Layout/HeaderCartButton.js
+++ technologies/react/09-practice-project/src/components/Layout/HeaderCartButton.js
@@ -1,4 +1,4 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../contexts/cart-context";
@@ -6,7 +6,11 @@ import CartContext from "../../contexts/cart-context";
 import classes from "./HeaderCartButton.module.css";
 
 function HeaderCartButton(props) {
-  const CartCtx = useContext(CartContext)
+  const CartCtx = useContext(CartContext);
+
+  const totalItemsAmount = CartCtx.items.reduce((total, item) => {
+    return total + item.amount;
+  }, 0);
 
   return (
     <button onClick={props.onClick} className={classes.button}>
@@ -14,7 +18,7 @@ function HeaderCartButton(props) {
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>{CartCtx.totalAmount}</span>
+      <span className={classes.badge}>{totalItemsAmount}</span>
     </button>
   );
 }
--- technologies/react/09-practice-project/src/contexts/CartProvider.js
+++ technologies/react/09-practice-project/src/contexts/CartProvider.js
@@ -1,22 +1,56 @@
-import React from "react";
+import React, { useReducer } from "react";
 import CartContext from "./cart-context";
 
 // this is a provider component
 // so any children component that needs access to CartContext
 // will consume from this component
 // we will also store all the logics from cart management here
+
+const defaultCart = {
+  items: [],
+  totalAmount: 0,
+};
+
+const cartReducer = (state, action) => {
+  if (action.type === "ADD_ITEM") {
+    // the reason why we use concat instead of push
+    // is because we want to create a brand new array instead of updating
+    // previous state
+    const updatedItems = state.items.concat(action.item);
+    const newTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount;
+    return {
+      items: updatedItems,
+      totalAmount: newTotalAmount,
+    };
+  }
+
+  if (action.type === "REMOVE_ITEM") {
+  }
+
+  return defaultCart;
+};
+
 function CartProvider(props) {
+  const [cartState, cartDispatcher] = useReducer(cartReducer, defaultCart);
+
   const addItemToCartHandler = (item) => {
-    console.log("ADDING ITEM");
+    cartDispatcher({
+      type: "ADD_ITEM",
+      item: item,
+    });
   };
 
   const removeItemFromCartHandler = (id) => {
-    console.log("REMOVING ITEM");
+    cartDispatcher({
+      type: "REMOVE_ITEM",
+      id: id,
+    });
   };
 
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
   };
