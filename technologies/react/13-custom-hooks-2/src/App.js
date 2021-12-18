5275c70d71f67a03fe596818e4548544cefa8821
David <davidphang431@gmail.com>
2021-12-18 16:55:42 +0700

    React Custom Hook HTTP The Better Version

--- technologies/react/13-custom-hooks-2/src/App.js
+++ technologies/react/13-custom-hooks-2/src/App.js
@@ -7,27 +7,25 @@ import NewTask from "./components/NewTask/NewTask";
 function App() {
   const [tasks, setTasks] = useState([]);
 
-  const transformTask = (taskObj) => {
-    const loadedTasks = [];
-
-    for (const taskKey in taskObj) {
-      loadedTasks.push({ id: taskKey, text: taskObj[taskKey].text });
-    }
-
-    setTasks(loadedTasks);
-  };
-
-  const httpData = useHttp(
-    {
-      url: "https://learn-react-c51fe-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json",
-    },
-    transformTask
-  );
-
-  const { isLoading, error, sendRequests: fetchTasks } = httpData;
+  const { isLoading, error, sendRequests: fetchTasks } = useHttp();
 
   useEffect(() => {
-    fetchTasks();
+    const transformTask = (taskObj) => {
+      const loadedTasks = [];
+
+      for (const taskKey in taskObj) {
+        loadedTasks.push({ id: taskKey, text: taskObj[taskKey].text });
+      }
+
+      setTasks(loadedTasks);
+    };
+
+    fetchTasks(
+      {
+        url: "https://learn-react-c51fe-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json",
+      },
+      transformTask
+    );
   }, []);
 
   const taskAddHandler = (task) => {
