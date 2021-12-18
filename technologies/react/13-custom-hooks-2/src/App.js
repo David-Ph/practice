57c9e3f2aa896d71a072866a12f02d6b36685282
David <davidphang431@gmail.com>
2021-12-18 16:38:22 +0700

    React custom hook for GET request

--- technologies/react/13-custom-hooks-2/src/App.js
+++ technologies/react/13-custom-hooks-2/src/App.js
@@ -1,39 +1,30 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
+import useHttp from "./hooks/useHttp";
 
-import Tasks from './components/Tasks/Tasks';
-import NewTask from './components/NewTask/NewTask';
+import Tasks from "./components/Tasks/Tasks";
+import NewTask from "./components/NewTask/NewTask";
 
 function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
   const [tasks, setTasks] = useState([]);
 
-  const fetchTasks = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://react-http-6b4a6.firebaseio.com/tasks.json'
-      );
+  const transformTask = (taskObj) => {
+    const loadedTasks = [];
 
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
-
-      const data = await response.json();
-
-      const loadedTasks = [];
-
-      for (const taskKey in data) {
-        loadedTasks.push({ id: taskKey, text: data[taskKey].text });
-      }
-
-      setTasks(loadedTasks);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
+    for (const taskKey in taskObj) {
+      loadedTasks.push({ id: taskKey, text: taskObj[taskKey].text });
     }
-    setIsLoading(false);
+
+    setTasks(loadedTasks);
   };
+
+  const httpData = useHttp(
+    {
+      url: "https://learn-react-c51fe-default-rtdb.asia-southeast1.firebasedatabase.app/tasks.json",
+    },
+    transformTask
+  );
+
+  const { isLoading, error, sendRequests: fetchTasks } = httpData;
 
   useEffect(() => {
     fetchTasks();
---
+++ technologies/react/note3-customhooks.js
@@ -0,0 +1,7 @@
+/* 
+? Custom hook?
+it has to have a "use" in front of it
+
+When we call custom hook in a component, a state that was initialized in that custom hook will be tied to that component
+
+*/
