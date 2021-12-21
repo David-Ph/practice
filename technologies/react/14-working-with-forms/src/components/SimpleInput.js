32f72ae6c590a8aa6ac4346e51ca3990c0ad9099
David <davidphang431@gmail.com>
2021-12-21 08:51:45 +0700

    React basic ways to handle form submission

--- technologies/react/14-working-with-forms/src/components/SimpleInput.js
+++ technologies/react/14-working-with-forms/src/components/SimpleInput.js
@@ -1,15 +1,38 @@
-const SimpleInput = (props) => {
+import { useState, useRef } from "react";
+
+function SimpleInput(props) {
+  const nameInputRef = useRef();
+  const [enteredName, setEnteredName] = useState("");
+
+  const nameInputHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+    // console.log(enteredName);
+    // console.log(nameInputRef.current.value);
+    setEnteredName('');
+    // nameInputRef.current.value = "" => NOT IDEAL, DON'T TRY TO MANIPULATE THE DOM
+  };
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSubmitHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          type="text"
+          id="name"
+          onChange={nameInputHandler}
+          value={enteredName}
+          ref={nameInputRef}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
       </div>
     </form>
   );
-};
+}
 
 export default SimpleInput;
