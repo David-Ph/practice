04c1f7ea2de4522a5c75da914152112076b444fd
David <davidphang431@gmail.com>
2021-12-29 09:33:42 +0700

    React handing http states and feedback with redux

--- technologies/react/18-redux-advance/src/App.js
+++ technologies/react/18-redux-advance/src/App.js
@@ -1,30 +1,83 @@
-import { useEffect } from "react";
-import { useSelector } from "react-redux";
+import { useEffect, Fragment } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
+import Notification from "./components/UI/Notification";
+import { uiActions } from "./store/ui-slice";
+
+let isInitial = true;
 
 function App() {
+  const dispatch = useDispatch();
   const cartIsVisible = useSelector((state) => state.ui.cartIsVisible);
   // becaause useSelecto sets up a subscription with the store
   // whenever the store changes
   // this component will also be reevaluated
   // and useEffect will run again, and it will update the cart again
   const cart = useSelector((state) => state.cart);
+  const notification = useSelector((state) => state.ui.notification);
 
   useEffect(() => {
-    fetch(
-      "https://learn-react-c51fe-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json",
-      { method: "PUT", body: JSON.stringify(cart) }
-    );
-  }, [cart]);
+    const sendData = async () => {
+      dispatch(
+        uiActions.setNotification({
+          status: "pending",
+          title: "Sending...",
+          message: "Sending cart data...",
+        })
+      );
+      const response = await fetch(
+        "https://learn-react-c51fe-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json",
+        { method: "PUT", body: JSON.stringify(cart) }
+      );
+
+      if (!response.ok) {
+        throw new Error("Sending cart data failed");
+      }
+
+      dispatch(
+        uiActions.setNotification({
+          status: "success",
+          title: "Success!",
+          message: "Send cart data success!",
+        })
+      );
+
+      const data = await response.json();
+    };
+
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+
+    sendData().catch((err) => {
+      dispatch(
+        uiActions.setNotification({
+          status: "error",
+          title: "Error!",
+          message: "Send cart data failed!",
+        })
+      );
+    });
+  }, [cart, dispatch]);
 
   return (
-    <Layout>
-      {cartIsVisible && <Cart />}
-      <Products />
-    </Layout>
+    <Fragment>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {cartIsVisible && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
