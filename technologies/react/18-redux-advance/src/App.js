90f5058e8b613b833f18412b46b2fa89453f60b5
David <davidphang431@gmail.com>
2021-12-29 09:18:57 +0700

    React using useEffect with redux

--- technologies/react/18-redux-advance/src/App.js
+++ technologies/react/18-redux-advance/src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
 
 import Cart from "./components/Cart/Cart";
@@ -6,6 +7,18 @@ import Products from "./components/Shop/Products";
 
 function App() {
   const cartIsVisible = useSelector((state) => state.ui.cartIsVisible);
+  // becaause useSelecto sets up a subscription with the store
+  // whenever the store changes
+  // this component will also be reevaluated
+  // and useEffect will run again, and it will update the cart again
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    fetch(
+      "https://learn-react-c51fe-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json",
+      { method: "PUT", body: JSON.stringify(cart) }
+    );
+  }, [cart]);
 
   return (
     <Layout>
