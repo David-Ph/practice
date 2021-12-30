c0b7f47510cc59e2c4db0c75db3976eb3155a169
David <davidphang431@gmail.com>
2021-12-30 09:20:47 +0700

    React defining and using routes

--- technologies/react/19-react-router/src/App.js
+++ technologies/react/19-react-router/src/App.js
@@ -1,6 +1,17 @@
+import { Route } from "react-router-dom";
+
+import Welcome from "./pages/Welcome";
+import Product from "./pages/Product";
+
 function App() {
   return (
     <div>
+      <Route path="/welcome">
+        <Welcome />
+      </Route>
+      <Route path="/products">
+        <Product />
+      </Route>
       <h2>Let's get started!</h2>
     </div>
   );
---
+++ technologies/react/note5-react-router.js
@@ -0,0 +1,7 @@
+/* 
+Todo: Steps to use react router
+    1. install react-router-dom@5
+    2. create the new pages
+    3. in App.js, import {Route} and set up the routes with the necessary components
+    4. in index.js, import {BrowserRouter} and wrap App.js in it
+*/
