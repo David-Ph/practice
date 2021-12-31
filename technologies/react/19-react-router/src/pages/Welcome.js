b22318ba078041051476c66cc137f3531ffa8d15
David <davidphang431@gmail.com>
2021-12-31 09:15:02 +0700

    React nested routes

--- technologies/react/19-react-router/src/pages/Welcome.js
+++ technologies/react/19-react-router/src/pages/Welcome.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import { Route } from "react-router-dom";
+import React from "react";
 
 function Welcome() {
-    return (
-        <div>
-            <h1>Welcome page</h1>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Welcome page</h1>
+      <Route path="/welcome/new-user">
+        <p>Welcome, new user!</p>
+      </Route>
+    </div>
+  );
 }
 
-export default Welcome
+export default Welcome;
