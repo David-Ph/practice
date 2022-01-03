222fcfbfd4099e52dd5be5609ba8f125ed9ea4af
David <davidphang431@gmail.com>
2022-01-03 09:11:09 +0700

    React router outputting data on details page

--- technologies/react/20-react-router-practice/src/pages/QuoteDetail.js
+++ technologies/react/20-react-router-practice/src/pages/QuoteDetail.js
@@ -1,15 +1,27 @@
 import { useParams, Route } from "react-router-dom";
 import React from "react";
 
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
 import Comments from "../components/comments/Comments";
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: "MaoMao", text: "Learning React" },
+  { id: "q2", author: "Captain", text: "To be or not to be" },
+  { id: "q3", author: "Huya", text: "Random stuff" },
+];
 
 function QuoteDetail() {
   const { quoteId } = useParams();
 
+  const findQuote = DUMMY_QUOTES.find((quote) => quote.id === quoteId);
+
+  if (!findQuote) {
+    return <h2>No Quote Found</h2>;
+  }
+
   return (
     <div>
-      <h1>Quote Detail</h1>
-      <h3>This is quote {quoteId}</h3>
+      <HighlightedQuote author={findQuote.author} text={findQuote.text} />
       <Route path={`/quotes/${quoteId}/comments`}>
         <Comments />
       </Route>
