e19bad199fba51cedc63a11e250e3c8281fcaae2
David <davidphang431@gmail.com>
2022-01-06 08:56:59 +0700

    React deployment

--- technologies/react/note7-deploying-react.js
+++ technologies/react/note7-deploying-react.js
@@ -24,4 +24,17 @@ We need to wrap the part of the code that does the routing in Suspense component
 ?      </Suspense>
 ?    </Layout>
 now, while we're waiting for the new page to load, it will show the loadingspinner component
+
+todo: build
+run this command to build/compile the code for production where all the codes is turned to javascript code for production
+? npm run build
+
+todo: what kind of hosting do we need?
+For React SPA application, at the end of the day all we need is a host that can host a static page website
+because after all that react code is build to javascript code, all we have is javascript, there is no server side code.
+
+todo: server side routing vs client side routing
+react-router-dom is a browser/client-side routing.
+server-side routing is when you send a request to the server, and then the server took a look at the url/path from the request, and then it will send a different file depending on the url. with react SPA, we don't want to do that, we want the server to always send the same file everytime.
+For react SPA, you need to configure your server so it works with SPA.
 */
