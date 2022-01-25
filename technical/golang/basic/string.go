05c3c7e090deb27addea4bd02c566c8ad1125140
David <davidphang431@gmail.com>
2022-01-25 16:34:24 +0700

    Go Data type

---
+++ technical/golang/basic/string.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("My name is MaoMao")
+	fmt.Println("My name is MaoMao")
+	fmt.Println(`My name is MaoMao`)
+	fmt.Println(len("string"))
+	fmt.Println("string"[2])
+}
