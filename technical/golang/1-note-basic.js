9b46c2c8449b29a12b9bad3f726a6da658baafd1
David <davidphang431@gmail.com>
2022-01-25 15:45:55 +0700

    Golang hello world

---
+++ technical/golang/1-note-basic.js
@@ -0,0 +1,18 @@
+/* 
+go works in a similar way with C
+
+in that you have to compile go program 
+and in program you have to import package to do anything
+
+? import 'fmt'
+Package fmt implements formatted I/O with functions analogous to C's printf
+
+? go build ./helloWorld.go
+this is the command to compile go app
+this will compile to something like .exe
+
+? go run ./helloworld.go
+this is something like npx nodemon helloworld.go
+this is for development
+
+*/
