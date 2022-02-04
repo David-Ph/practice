2f58ca2a09f3a82b41b79a59b7e97c30c0842dde
David <davidphang431@gmail.com>
2022-02-04 18:41:52 +0700

    Add sumTO

---
+++ technical/codewars/second/7_3_sumTo.js
@@ -0,0 +1,18 @@
+function getSum(a, b) {
+  if (a === b) return a;
+
+  let total = 0;
+  if (a > b) {
+    for (let i = a; i >= b; i--) {
+      total += i;
+    }
+  } else {
+    for (let i = a; i <= b; i++) {
+      total += i;
+    }
+  }
+
+  return total;
+}
+
+console.log(getSum(0, 1));
