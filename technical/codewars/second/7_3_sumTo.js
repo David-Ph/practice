95c8990a0de033517e5a7066ef1f6e8548be6990
David <davidphang431@gmail.com>
2022-02-07 10:44:08 +0700

    Add isSquare

--- technical/codewars/second/7_3_sumTo.js
+++ technical/codewars/second/7_3_sumTo.js
@@ -1,18 +1,18 @@
 function getSum(a, b) {
-  if (a === b) return a;
+    if (a === b) return a;
 
-  let total = 0;
-  if (a > b) {
-    for (let i = a; i >= b; i--) {
-      total += i;
+    let total = 0;
+    if (a > b) {
+        for (let i = a; i >= b; i--) {
+        total += i;
+        }
+    } else {
+        for (let i = a; i <= b; i++) {
+        total += i;
+        }
     }
-  } else {
-    for (let i = a; i <= b; i++) {
-      total += i;
-    }
-  }
 
-  return total;
+    return total;
 }
 
 console.log(getSum(0, 1));
