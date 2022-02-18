cb7ad983547ae4ea826a4d6bab89bc758e56b6ac
David <davidphang431@gmail.com>
2022-02-18 18:05:58 +0700

    Golang modules

--- technical/golang/src/belajar-modules/go.mod
+++ technical/golang/src/belajar-modules/go.mod
@@ -1,3 +1,5 @@
 module belajar-modules
 
 go 1.17
+
+require github.com/ProgrammerZamanNow/go-say-hello v1.5.0
--- technical/golang/src/belajar-modules/main.go
+++ technical/golang/src/belajar-modules/main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"belajar-modules/lib"
 	"fmt"
+
+	go_say_hello "github.com/ProgrammerZamanNow/go-say-hello"
 )
 
 func main() {
 	result := lib.ConcatenateString("My", "String")
 	fmt.Println(result)
+	fmt.Println(go_say_hello.SayHello())
 }
