4db42a5270414b6a88f5f1e67f5ea8f7f7868fa3
David <davidphang431@gmail.com>
2022-03-11 18:05:31 +0700

    golang parent and child context

--- technical/golang/context.js
+++ technical/golang/context.js
@@ -27,4 +27,27 @@ Membuat context kosong. Tidak pernah dibatalkan, tidak pernah timeout, dan tidak
 ? context.TODO()
 Membuat context kosong seperti Background(), namun biasanya menggunakan ini ketika belum jelas context apa yang ingin digunakan
 
+* Parent dan Child Context
+Context menganut konsep parent dan child
+Artinya, saat kita membuat context, kita bisa membuat child context dari context yang sudah ada
+Parent context bisa memiliki banyak child, namun child hanya bisa memiliki satu parent context
+Konsep ini mirip dengan pewarisan di pemrograman berorientasi object
+
+* Hubungan Antara Parent dan Child Context
+Parent dan Child context akan selalu terhubung
+Saat nanti kita melakukan misal pembatalan context A, maka semua child dan sub child dari context A akan ikut dibatalkan
+Namun jika misal kita membatalkan context B, hanya context B dan semua child dan sub child nya yang dibatalkan, parent context B tidak akan ikut dibatalkan
+Begitu juga nanti saat kita menyisipkan data ke dalam context A, semua child dan sub child nya bisa mendapatkan data tersebut
+Namun jika kita menyisipkan data di context B, hanya context B dan semua child dan sub child nya yang mendapat data, parent context B tidak akan mendapat data
+
+* Immutable
+Context merupakan object yang Immutable, artinya setelah Context dibuat, dia tidak bisa diubah lagi
+Ketika kita menambahkan value ke dalam context, atau menambahkan pengaturan timeout dan yang lainnya, secara otomatis akan membentuk child context baru, bukan merubah context tersebut
+
+* Context With Value
+Pada saat awal membuat context, context tidak memiliki value
+Kita bisa menambah sebuah value dengan data Pair (key - value) ke dalam context
+Saat kita menambah value ke context, secara otomatis akan tercipta child context baru, artinya original context nya tidak akan berubah sama sekali
+Untuk membuat menambahkan value ke context, kita bisa menggunakan function context.WithValue(parent, key, value)
+
 */
