08b80aa2e3e720e5dad26bc370a74820259c620f
David <davidphang431@gmail.com>
2022-03-13 15:08:58 +0700

    how to measure speed?

--- technical/javascript/dsa/big_o_notation/1_intro.js
+++ technical/javascript/dsa/big_o_notation/1_intro.js
@@ -5,6 +5,8 @@ write a function that calculates the sum of all numbers from 1 up to n
 // approach 1
 // using simple loop
 // the complexity of this is O(n)
+// the problem with this problem is
+// the larger n is, the longer this block of code takes.
 function addUpTo(n) {
   let total = 0;
   for (let i = 1; i < n; i++) {
@@ -17,6 +19,8 @@ function addUpTo(n) {
 // using math formula
 // the pros of this is, it's not using a loop so this is probably very fast
 // the cons of this is, it's not very intuitive
+// with this solution, it doesn't matter how big n is
+// it will always do the same number of operations
 function addUpToFormula(n) {
   return (n * (n + 1)) / 2;
 }
--- technical/javascript/dsa/big_o_notation/1_introNode.js
+++ technical/javascript/dsa/big_o_notation/1_introNode.js
@@ -9,6 +9,9 @@ Useful for discussing trade-offs between different approaches
 When your code slows down or crashes, identifying parts of the code that are inefficient can help us find pain points in our applications
 Less important: it comes up in interviews!
 
+so the problem with using time to measure speed of a block of code
+is that it's unreliable
+because different commputer may have different speed
 
-
+another way we can check it is how many operations does it do?
 */
