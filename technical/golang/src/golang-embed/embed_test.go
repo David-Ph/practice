d8c1d5397f48563be1bdba3adec726bd93965c80
David <davidphang431@gmail.com>
2022-03-26 17:43:48 +0700

    Golang embed on []byte

--- technical/golang/src/golang-embed/embed_test.go
+++ technical/golang/src/golang-embed/embed_test.go
@@ -3,13 +3,25 @@ package golang_embed
 import (
 	_ "embed"
 	"fmt"
+	"io/fs"
+	"io/ioutil"
 	"testing"
 )
 
 //go:embed version.txt
 var version string
 
+//go:embed logo.png
+var logo []byte
+
 func TestString(t *testing.T) {
 	fmt.Println("Printing version")
 	fmt.Println(version)
+}
+
+func TestPng(t *testing.T) {
+	err := ioutil.WriteFile("logo_next.png", logo, fs.ModePerm)
+	if err != nil {
+		panic(err)
+	}
 }
