50b77d4b9bf72d838b688474d055d358aacfff36
David <davidphang431@gmail.com>
2022-03-26 18:28:37 +0700

    HTTP message and url

---
+++ technical/web/http/05-http-message.js
@@ -0,0 +1,12 @@
+/* 
+* HTTP Message
+HTTP Request dan HTTP Response, sebenarnya adalah sebuah HTTP Message
+HTTP Message memiliki standarisasi format 
+Dengan demikian, jika kita ingin membuat Client dan Server sendiri, sebenarnya bisa kita lakukan, asal kita mengikuti standarisasi format HTTP Message
+
+* HTTP Method
+Dalam HTTP Request, hal yang pertama kita perlu tentukan adalah HTTP Method
+HTTP Method mirip seperti kategori request
+Ada banyak HTTP Method yang dapat kita gunakan ketika membuat HTTP Request, dan kita bisa sesuaikan sesuai dengan kebutuhan yang kita inginkan
+
+*/
