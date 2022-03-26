9c24159d42c15a923f73b308f778713f8e739843
David <davidphang431@gmail.com>
2022-03-26 18:30:19 +0700

    http header

---
+++ technical/web/http/07-http-header.js
@@ -0,0 +1,25 @@
+/* 
+* HTTP Header
+HTTP Header merupakan informasi tambahan yang biasa dikirim di Request atau Response
+HTTP Header biasanya digunakan agar informasi tidak harus dikirim melalui Request Body atau Response Body
+HTTP Header berisi key : value, dan saat ini sudah banyak sekali standarisasi nama key pada HTTP Header
+https://en.wikipedia.org/wiki/List_of_HTTP_header_fields 
+
+* Contoh HTTP Header
+ Host
+Authority pada URL (wajib sejak versi HTTP/1.1)
+
+Content-Type
+Tipe data dari HTTP Body
+
+User-Agent
+Informasi user agent (seperti browser dan sistem operasi)
+
+Accept
+Tipe data yang diterima oleh Client
+
+Authorization
+Credential untuk autentikasi (misal username + password)
+
+
+*/
