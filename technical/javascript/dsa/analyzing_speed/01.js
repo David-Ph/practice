8cff37a2790608487908dcfee4f38cc9c2f5cc9e
David <davidphang431@gmail.com>
2022-03-27 15:54:56 +0700

    Speed of javascript arrays and objects

---
+++ technical/javascript/dsa/analyzing_speed/01.js
@@ -0,0 +1,7 @@
+const instructor = {
+  firstName: "MaoMao",
+  isMarried: true,
+  favoriteNumbers: [1, 2, 3, 4],
+};
+
+console.log(Object.keys(instructor)); // ['firstName', 'isMarried', 'favoriteNumbers']
