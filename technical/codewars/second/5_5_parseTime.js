caf4acb7e58809a48441dec4a213c225fefc80b9
David <davidphang431@gmail.com>
2022-03-30 10:09:04 +0700

    Add parseTime

---
+++ technical/codewars/second/5_5_parseTime.js
@@ -0,0 +1,13 @@
+const parseTime = (timeString = "") => {
+  if (timeString === "") return null;
+  const d = new Date();
+  const time = timeString.match(/^(\d\d?)(?::?(\d\d))?(?::(\d\d))/i);
+
+  d.setHours(parseInt(time[1], 10) || 0);
+  d.setMinutes(parseInt(time[2], 10) || 0);
+  d.setSeconds(0, 0);
+
+  return d;
+};
+
+console.log(parseTime("23:00:00"));
