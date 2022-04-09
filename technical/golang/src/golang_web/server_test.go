b7b2763bffc1437da0cb755e07ae9e4fedd25cef
David <davidphang431@gmail.com>
2022-04-09 18:11:22 +0700

    Golang web cookie

--- technical/golang/src/golang_web/server_test.go
+++ technical/golang/src/golang_web/server_test.go
@@ -233,3 +233,68 @@ func TestResponseCode(t *testing.T) {
 	fmt.Println(response.Status)
 	fmt.Println(string(body))
 }
+
+// cookie
+func SetCookie(writer http.ResponseWriter, request *http.Request) {
+	cookie := new(http.Cookie)
+	cookie.Name = "X-PZN-Name"
+	cookie.Value = request.URL.Query().Get("name")
+	cookie.Path = "/"
+
+	http.SetCookie(writer, cookie)
+	fmt.Fprint(writer, "Success Create Cookie")
+}
+
+func GetCookie(writer http.ResponseWriter, request *http.Request) {
+	cookie, err := request.Cookie("X-PZN-Name")
+	if err != nil {
+		fmt.Fprint(writer, "No Cookie")
+	} else {
+		fmt.Fprintf(writer, "Hello %s", cookie.Value)
+	}
+}
+
+func TestCookie(t *testing.T) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/set-cookie", SetCookie)
+	mux.HandleFunc("/get-cookie", GetCookie)
+
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: mux,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func TestCreatingCookie(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost/?name=MaoMao", nil)
+	recorder := httptest.NewRecorder()
+
+	SetCookie(recorder, request)
+
+	cookies := recorder.Result().Cookies()
+
+	for _, cookie := range cookies {
+		fmt.Printf("%s : %s\n", cookie.Name, cookie.Value)
+	}
+}
+
+func TestGettingCookie(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost/", nil)
+	cookie := new(http.Cookie)
+	cookie.Name = "X-PZN-Name"
+	cookie.Value = "Fey"
+	request.AddCookie(cookie)
+
+	recorder := httptest.NewRecorder()
+
+	GetCookie(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+	fmt.Println(string(body))
+}
