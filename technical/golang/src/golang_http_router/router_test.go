cc567822fd1ec16644d6545d59f24b3b108a0040
David <davidphang431@gmail.com>
2022-04-25 17:34:30 +0700

    Golang httprouter router pattern

--- technical/golang/src/golang_http_router/router_test.go
+++ technical/golang/src/golang_http_router/router_test.go
@@ -45,3 +45,39 @@ func TestParams(t *testing.T) {
 
 	assert.Equal(t, "Product 322", string(body))
 }
+
+func TestNamedParameter(t *testing.T) {
+	router := httprouter.New()
+	router.GET("/products/:id/items/:itemId", func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+		text := "Product " + p.ByName("id") + " Item " + p.ByName("itemId")
+		fmt.Fprint(w, text)
+	})
+
+	request := httptest.NewRequest("GET", "http://localhost:8080/products/1/items/2", nil)
+	recorder := httptest.NewRecorder()
+
+	router.ServeHTTP(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+
+	assert.Equal(t, "Product 1 Item 2", string(body))
+}
+
+func TestCatchAll(t *testing.T) {
+	router := httprouter.New()
+	router.GET("/images/*image", func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+		text := "Image: " + p.ByName("image")
+		fmt.Fprint(w, text)
+	})
+
+	request := httptest.NewRequest("GET", "http://localhost:8080/images/small/profile.png", nil)
+	recorder := httptest.NewRecorder()
+
+	router.ServeHTTP(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+
+	assert.Equal(t, "Image: /small/profile.png", string(body))
+}
