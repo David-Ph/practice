e54ba253a8e7a7c37b904bdb20e52666211cd882
David <davidphang431@gmail.com>
2022-05-18 15:43:59 +0700

    React Next JS extracting the parameter

--- technologies/react/24-nextjs/nextjs-course/pages/news/index.js
+++ technologies/react/24-nextjs/nextjs-course/pages/news/index.js
@@ -1,5 +1,16 @@
 // domain.com/news
+import Link from "next/link";
 
 export default function Newspage(params) {
-  return <h1>News Page</h1>;
+  return (
+    <>
+      <h1>News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/nextjs">NextJS is a great framework</Link>
+        </li>
+        <li>Something Else</li>
+      </ul>
+    </>
+  );
 }
