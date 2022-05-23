e5c7a49fe97729cbbd6fb0531a9e896d1de0b70a
David <davidphang431@gmail.com>
2022-05-23 17:16:54 +0700

    Goland DI Provider Set

--- technical/golang/src/belajar-golang-restful-api/simple/wire_gen.go
+++ technical/golang/src/belajar-golang-restful-api/simple/wire_gen.go
@@ -6,6 +6,10 @@
 
 package simple
 
+import (
+	"github.com/google/wire"
+)
+
 // Injectors from injector.go:
 
 func CreateService(isError bool) (*SimpleService, error) {
@@ -23,3 +27,18 @@ func InitializeDatabase() *DatabaseRepository {
 	databaseRepository := NewDatabaseRepository(databaseMongoDB, databasePostgreSQL)
 	return databaseRepository
 }
+
+func InitializedFooBarService() *FooBarService {
+	fooRepository := NewFooRepository()
+	fooService := NewFooService(fooRepository)
+	barRepository := NewBarRepository()
+	barService := NewBarService(barRepository)
+	fooBarService := NewFooBarService(fooService, barService)
+	return fooBarService
+}
+
+// injector.go:
+
+var fooSet = wire.NewSet(NewFooRepository, NewFooService)
+
+var barSet = wire.NewSet(NewBarRepository, NewBarService)
