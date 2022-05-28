7f0b180216a38517cf8d307ad2c798b4138da45c
David <davidphang431@gmail.com>
2022-05-28 10:26:50 +0700

    React NextJS custom components and css modules

---
+++ technologies/react/24-nextjs/practice-project/pages/[meetupId]/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+export default function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://i.pinimg.com/originals/a2/54/41/a25441888989bf8650f2de212c7c0b43.jpg"
+      title="A First Meetup"
+      address="Some Street 5, Some City"
+      description="The Meetup Description"
+    />
+  );
+}
