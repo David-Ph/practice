00d439e60d4144cee12c9adedb1070b17aa468f8
David <davidphang431@gmail.com>
2022-05-28 10:46:31 +0700

    React NextJS data fetching for static pages

--- technologies/react/24-nextjs/practice-project/pages/index.js
+++ technologies/react/24-nextjs/practice-project/pages/index.js
@@ -35,6 +35,24 @@ const DUMMY_MEETUPS = [
   },
 ];
 
-export default function HomePage() {
-  return <MeetupList meetups={DUMMY_MEETUPS} />;
+export default function HomePage(props) {
+  return <MeetupList meetups={props.meetups} />;
+}
+
+// Normally, if we want to load a react page
+// we will use useState and useEffect to dynamically fetch data and states
+// the problem with this is it't not good for SEO
+// this is why nextjs helps us with the function getStaticProps()
+
+export async function getStaticProps() {
+  // code from here will never reach the end user
+  // because code here is only executed during build time
+  // so we can do async call here
+  // this function has to return an object, and it has to have props item in it
+  // this props will be sent to the main component in this page
+  return {
+    props: {
+      meetups: DUMMY_MEETUPS,
+    },
+  };
 }
