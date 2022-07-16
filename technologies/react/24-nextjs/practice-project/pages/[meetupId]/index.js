2525e0e09401f5bf0e27cb75e5e0ac7538e18f59
David <davidphang431@gmail.com>
2022-07-16 12:17:08 +0700

    NextJS getting meetup details

--- technologies/react/24-nextjs/practice-project/pages/[meetupId]/index.js
+++ technologies/react/24-nextjs/practice-project/pages/[meetupId]/index.js
@@ -1,66 +1,64 @@
+import { MongoClient, ObjectId } from "mongodb";
 import React from "react";
 import MeetupDetail from "../../components/meetups/MeetupDetail";
 
 export default function MeetupDetails(props) {
   return (
     <MeetupDetail
-      image="https://i.pinimg.com/originals/a2/54/41/a25441888989bf8650f2de212c7c0b43.jpg"
-      title="A First Meetup"
-      address="Some Street 5, Some City"
-      description="The Meetup Description"
+      image={props.meetup.image}
+      title={props.meetup.title}
+      address={props.meetup.address}
+      description={props.meetup.description}
     />
   );
 }
 
-export function getStaticPaths() {
+export async function getStaticPaths() {
   // we need this function if we're loading a dynamic page AND we're using getStaticProps
   // basically what we need to do here is to load up all the possible dynamic values
   // that can be loaded here
   // so in this case, we need to maybe fetch all the meetupId from database
   // but for now let's just hard code it
+
+  // Fetch data from API
+  const client = new MongoClient(process.env.MONGO_URI);
+  await client.connect();
+  const database = client.db("nextjs_meetups");
+
+  const meetupsCollections = database.collection("meetups");
+  const result = await meetupsCollections.find({}, { _id: 1 }).toArray();
+  client.close();
   return {
     // what is fallback here?
     // if set to false, if we load into a page where the id is not found here
     // it well return a 404 page
     // if set to true, it will attempt to render the page from server
     fallback: false,
-    paths: [
-      {
-        params: {
-          meetupId: "1",
-        },
-      },
-      {
-        params: {
-          meetupId: "2",
-        },
-      },
-      {
-        params: {
-          meetupId: "3",
-        },
-      },
-      {
-        params: {
-          meetupId: "4",
-        },
-      },
-    ],
+    paths: result.map((meetup) => ({
+      params: { meetupId: meetup._id.toString() },
+    })),
   };
 }
-export function getStaticProps(context) {
+export async function getStaticProps(context) {
   const { params } = context;
   const { meetupId } = params;
+
+  const client = new MongoClient(process.env.MONGO_URI);
+  await client.connect();
+  const database = client.db("nextjs_meetups");
+
+  const meetupsCollections = database.collection("meetups");
+  const result = await meetupsCollections.findOne({ _id: ObjectId(meetupId) });
+  client.close();
 
   return {
     props: {
       meetup: {
-        id: meetupId,
-        image:
-          "https://i.pinimg.com/originals/a2/54/41/a25441888989bf8650f2de212c7c0b43.jpg",
-        title: "A First Meetup",
-        address: "Some Street 5, Some City",
-        description: "The Meetup Description",
+        id: result._id.toString(),
+        title: result.title,
+        image: result.image,
+        description: result.description,
+        address: result.address,
       },
     },
   };
--- technologies/react/24-nextjs/practice-project/pages/index.js
+++ technologies/react/24-nextjs/practice-project/pages/index.js
@@ -6,41 +6,6 @@ import { MongoClient } from "mongodb";
 // Component
 import MeetupList from "../components/meetups/MeetupList";
 
-const DUMMY_MEETUPS = [
-  {
-    id: 1,
-    title: "A first Meetup",
-    address: "Some Address 12345 Some City",
-    description: "This is our first meetup!",
-    image:
-      "https://i.pinimg.com/originals/a2/54/41/a25441888989bf8650f2de212c7c0b43.jpg",
-  },
-  {
-    id: 2,
-    title: "A second Meetup",
-    address: "Some Address 12345 Some City",
-    description: "This is our second meetup!",
-    image:
-      "https://i.pinimg.com/originals/a2/54/41/a25441888989bf8650f2de212c7c0b43.jpg",
-  },
-  {
-    id: 3,
-    title: "A third Meetup",
-    address: "Some Address 12345 Some City",
-    description: "This is our third meetup!",
-    image:
-      "https://i.pinimg.com/originals/a2/54/41/a25441888989bf8650f2de212c7c0b43.jpg",
-  },
-  {
-    id: 4,
-    title: "A fourth Meetup",
-    address: "Some Address 12345 Some City",
-    description: "This is our fourth meetup!",
-    image:
-      "https://i.pinimg.com/originals/a2/54/41/a25441888989bf8650f2de212c7c0b43.jpg",
-  },
-];
-
 export default function HomePage(props) {
   return <MeetupList meetups={props.meetups} />;
 }
