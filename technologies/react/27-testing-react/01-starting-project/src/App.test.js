98e284323d49c801f8187b86fa0abc81bcd4d984
David <davidphang431@gmail.com>
2023-01-05 16:48:59 +0700

    Running test

--- technologies/react/27-testing-react/01-starting-project/src/App.test.js
+++ technologies/react/27-testing-react/01-starting-project/src/App.test.js
@@ -1,8 +1,16 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+// this is a test() function
+// it accepts 2 arguments
+// the first one is the test name
+// the second one is a function, this is where we put what to test
 test('renders learn react link', () => {
+  // We render the App Component
+  // this basically simulates a browser
   render(<App />);
+  // We query for an element in the screen
   const linkElement = screen.getByText(/learn react/i);
+  // then we check if the element exists in the document
   expect(linkElement).toBeInTheDocument();
 });
