90a9a462c061972c96ad5a378e4449f4e176f64f
David <davidphang431@gmail.com>
2023-01-06 17:11:08 +0700

    Working with mocks

--- technologies/react/27-testing-react/01-starting-project/src/components/Async.test.js
+++ technologies/react/27-testing-react/01-starting-project/src/components/Async.test.js
@@ -3,9 +3,22 @@ import Async from "./Async";
 
 describe("Async component", () => {
   test("renders post if request succeeds", async () => {
+    window.fetch = jest.fn();
+    window.fetch.mockResolvedValueOnce({
+      json: async () => [
+        {
+          id: 1,
+          title: "first post",
+        },
+      ],
+    });
     render(<Async />);
 
-    const listItems = await screen.findAllByRole("listitem");
+    const listItems = await screen.findAllByRole(
+      "listitem",
+      {},
+      { timeout: 5000 }
+    );
     expect(listItems).not.toHaveLength(0);
   });
 });
