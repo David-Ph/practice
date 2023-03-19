080ac46914066425f84a87e7e5ae6a4957f7b6cc
David <davidphang431@gmail.com>
2023-03-19 16:12:23 +0700

    Golang basic - creating new deck

--- technical/golang/src/102-cards/main.go
+++ technical/golang/src/102-cards/main.go
@@ -1,35 +1,33 @@
 package main
 
-import "fmt"
-
 // ? Function
-func newCard() string {
-	return "Five of Diamonds"
-}
+// func newCard() string {
+// 	return "Five of Diamonds"
+// }
 
 func main() {
 	// ? Variable Declarations
 	// var card string = "Ace of Spades"
-	card := "Ace of Spades"
-	card = "Five of Diamonds"
-	fmt.Println(card)
+	// card := "Ace of Spades"
+	// card = "Five of Diamonds"
+	// fmt.Println(card)
 
 	// ? Function
-	card2 := newCard()
-	fmt.Println(card2)
+	// card2 := newCard()
+	// fmt.Println(card2)
 
-	getState()
+	// getState()
 
 	// ? Array and slices
 	// Array is a fixed length list of things
 	// Slice is an array that can grow or shrink
 	// Every element in a slice must be of same type
-	cards := deck{"Ace of Spades", newCard()}
+	// cards := deck{"Ace of Spades", newCard()}
 
 	// appending slices
 	// the append function doesn't modify the original slice
 	// it instead returns a whole new slice
-	cards = append(cards, "Six of Spades")
+	// cards = append(cards, "Six of Spades")
 
 	// For loop
 	// for i, card := range cards {
@@ -40,5 +38,9 @@ func main() {
 	// fmt.Println(cards)
 
 	// calling deck method
+	// cards.print()
+
+	cards := newDeck()
+
 	cards.print()
 }
