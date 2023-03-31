347aa19819cd2af2fee13466bdd6dec09312790a
David <davidphang431@gmail.com>
2023-03-31 12:40:14 +0700

    Golang basic - interface assignment

---
+++ technical/golang/src/106-interface-assignment/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+type shape interface {
+	getArea() float64
+}
+
+func printArea(s shape) {
+	fmt.Println(s.getArea())
+}
+
+type triangle struct {
+	height float64
+	base   float64
+}
+type square struct {
+	sideLength float64
+}
+
+func (t triangle) getArea() float64 {
+	return 0.5 * t.base * t.height
+}
+
+func (s square) getArea() float64 {
+	return s.sideLength * s.sideLength
+}
+
+func main() {
+
+}
