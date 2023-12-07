449ff0edef447e53f64e789541ddd31cee747cd7
David <davidphang431@gmail.com>
2023-12-07 16:20:05 +0700

    Anon functions

--- technical/elixir/1/functions.md
+++ technical/elixir/1/functions.md
@@ -13,4 +13,13 @@ IO.puts(sumShortHand.(2, 3)) # 5
 say_hello = fn name -> "Hello, #{name}" end
 IO.puts(say_hello.("John"))
 
+say_hello_short = &("Hello, #{&1}. Greetings from #{&2}")
+IO.puts(say_hello_short.("John", "Bob"))
+
+handle_result = fn
+  {:ok, result} -> result
+  {:error, error} -> error
+end
+
+IO.puts(handle_result.({:ok, "success"}))
 ```
--- technical/elixir/1/hello_2.exs
+++ technical/elixir/1/hello_2.exs
@@ -1,19 +1,38 @@
-sum = fn a, b -> a + b end
+# sum = fn a, b -> a + b end
 
-IO.puts(sum.(2, 3)) # 5
+# IO.puts(sum.(2, 3)) # 5
 
-sumShortHand = &(&1 + &2)
-IO.puts(sumShortHand.(2, 3)) # 5
+# sumShortHand = &(&1 + &2)
+# IO.puts(sumShortHand.(2, 3)) # 5
 
-say_hello = fn name -> "Hello, #{name}" end
-IO.puts(say_hello.("John"))
+# say_hello = fn name -> "Hello, #{name}" end
+# IO.puts(say_hello.("John"))
 
-say_hello_short = &("Hello, #{&1}. Greetings from #{&2}")
-IO.puts(say_hello_short.("John", "Bob"))
+# say_hello_short = &("Hello, #{&1}. Greetings from #{&2}")
+# IO.puts(say_hello_short.("John", "Bob"))
 
-handle_result = fn
-  {:ok, result} -> result
-  {:error, error} -> error
+# handle_result = fn
+#   {:ok, result} -> result
+#   {:error, error} -> error
+# end
+
+# IO.puts(handle_result.({:ok, "success"}))
+
+defmodule Math do
+  def sum(num1, num2) do
+    num1 + num2
+  end
 end
 
-IO.puts(handle_result.({:ok, "success"}))
+defmodule Greet do
+  def hello(name) do
+    greeting(name)
+  end
+
+  defp greeting(name) do
+    "Hello, #{name}"
+  end
+end
+
+IO.puts(Math.sum(1, 2)) # 3
+IO.puts(Greet.hello("Bob"))
